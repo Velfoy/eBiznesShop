c2e9682b9f3f828edf27b5edc655b3718670a844
Darvel <marsonyteam@gmail.com>
2025-04-27 23:56:35 +0200

    Product card done basic visuals , animation is next step

--- src/pages/BestSellingProducts.jsx
+++ src/pages/BestSellingProducts.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import "../styles/BestSellingProducts.css";
-import { ShoppingCart } from "lucide-react";
+import imageb from "../assets/shoes.png";
+import arrowright from "../assets/arrow.png";
 
 const getProducts = () => {
   return Array(8).fill({
     title: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
     price: 2499.0,
     originalPrice: 4999.0,
-    image: "/shoes.png",
+    image: imageb,
     rating: 5,
+    link:"./products/is",
+    owner:"StarKist",
   });
 };
 
 const ProductCard = ({ product }) => (
   <div className="best_card">
     <span className="best_badge">New</span>
+    <button className="cart_add"><i class="fa-solid fa-cart-shopping"></i></button>
     <img src={product.image} alt={product.title} className="best_image" />
     <div className="best_card_content">
-      <h3 className="best_card_title">{product.title}</h3>
+      <div className="best_card_name_div">
+        <h3 className="best_card_title">{product.title}</h3>
+        <a href={product.link} className="best_card_link"><img alt="button_for_info" src={arrowright}/></a>
+      </div>
+      <div className="best_rating_d">
+        <div className="best_rating">{"★".repeat(product.rating)}</div>
+        <div className="best_rating_num">({product.rating}.0)</div>
+      </div>
+     <div className="best_owner">By <span>{product.owner}</span></div>
+    
       <div className="best_price_row">
         <span className="best_price">${product.price.toFixed(2)}</span>
         <span className="best_original_price">${product.originalPrice.toFixed(2)}</span>
       </div>
-      <div className="best_rating">{"★".repeat(product.rating)}</div>
-      <button className="best_add_button">
-        <ShoppingCart style={{ height: 16, width: 16, marginRight: 8 }} />
-        Add to Cart
-      </button>
     </div>
   </div>
 );
