82acc4e3e2abf2ca5546e8d2f91915e58672527f
Darvel <marsonyteam@gmail.com>
2025-05-02 01:04:19 +0200

    Home page first version done all of the objects , non-login and login user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 import HeroSlider from "./HeroSlider";
 import BestSellingProducts from "./BestSellingProducts";
 import HomeBottomInfo from "./HomeBottomInfo";
+import FeaturedCategories from "./FeaturedCategories";
+import RecommendedProducts from "./RecommendedProducts";
 
 const Home = () => {
   return (
     <div className="text-center" style={{"width":"100%"}}>
       <HeroSlider/>
+      <FeaturedCategories/>
+      <RecommendedProducts/>
       <BestSellingProducts/>
       <HomeBottomInfo/>
     </div>
