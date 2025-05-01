3074e0bfcd29281d7e81d634cc4e18c6f7fe1792
Darvel <marsonyteam@gmail.com>
2025-05-01 23:15:59 +0200

    Non login home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
 import HeroSlider from "./HeroSlider";
 import BestSellingProducts from "./BestSellingProducts";
+import HomeBottomInfo from "./HomeBottomInfo";
 
 const Home = () => {
   return (
     <div className="text-center" style={{"width":"100%"}}>
       <HeroSlider/>
       <BestSellingProducts/>
-      <p>Find the best fashion deals here.</p>
-      <Link to="/products" className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded">
-        Browse Products
-      </Link>
+      <HomeBottomInfo/>
     </div>
   );
 };
